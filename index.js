acef3dbf96e892563433cc462cc822b2d741e927
Ilona Petenko <petenko05@gmail.com>
2020-09-11 17:24:46 +0300

    Separated css and js from html.

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("back-button").hidden=true 
+document.getElementById("heart-img").style.visibility="hidden"
+
+document.getElementById("show-heart-button").onclick=function() { 
+    document.getElementById("heart-img").style.visibility="visible"
+    document.getElementById("back-button").hidden=false
+    document.getElementById("show-heart-button").hidden=true
+} 
+
+document.getElementById("back-button").onclick=function() { 
+    document.getElementById("show-heart-button").hidden=false
+    document.getElementById("heart-img").style.visibility="hidden"
+    document.getElementById("back-button").hidden=true
+}
